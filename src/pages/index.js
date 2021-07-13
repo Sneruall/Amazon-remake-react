7bab68d9bbffab39ad361b05e78d0f66f2081b75
Laurens <36268730+Sneruall@users.noreply.github.com>
2021-07-13 08:25:32 +0200

    Added a banner
    Using react-responsive-carousel npm package

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,20 @@
 import Head from "next/head";
+import Banner from "../components/Banner";
 import Header from "../components/Header";
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-grey-100">
       <Head>
         <title>Amazon 2.0</title>
       </Head>
-      {/* <Header/> */}
       <Header />
+
+      <main className=" max-w-screen-2xl mx-auto">
+        {/* Banner */}
+        <Banner />
+        {/* ProductFeed */}
+      </main>
     </div>
   );
 }
