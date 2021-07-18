b7537cd6748fda241e70cd400838062eb3bc2946
Laurens <36268730+Sneruall@users.noreply.github.com>
2021-07-18 09:11:47 +0200

    Shoppingcart counter
    Using redux store basketslice

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -1,13 +1,42 @@
 import Header from "../components/Header";
+import Image from "next/image";
+import { selectItems } from "../slices/basketSlice";
+import { useSelector } from "react-redux";
 
 function Checkout() {
-  return <div>
+  const items = useSelector(selectItems);
+  return (
+    <div>
       <div className="bg-gray-100">
-          <Header/>
+        <Header />
 
-          <h1>checkout page</h1>
+        <main className="lg:flex max-w-screen-2xl mx-auto">
+          {/* Left */}
+          <div className="flex-grow m-5 shadow-sm">
+            <Image
+              src="https://links.papareact.com/ikj"
+              width={1020}
+              height={250}
+              objectFit="contain"
+            />
+          </div>
+
+          <div className="flex flex-col p-5 space-y-10 bg-white">
+            <h1 className="text-3xl border-b pb-4">
+              {items.length === 0
+                ? "Your Amazon Basket is empty!"
+                : "Shopping Basket"}
+            </h1>
+            {items.map((item, i) => {
+              <CheckoutProduct />;
+            })}
+          </div>
+
+          {/* Right */}
+        </main>
       </div>
-  </div>;
+    </div>
+  );
 }
 
 export default Checkout;
