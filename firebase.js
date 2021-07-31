b8c0575bb5cad48b27ae3c15b0e7304eed230085
Laurens <36268730+Sneruall@users.noreply.github.com>
2021-07-31 14:58:50 +0200

    added stripe cli functionality with a webhook

--- firebase.js
+++ firebase.js
@@ -1,10 +1,10 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDafaMcf6XKsgWe95YZ898vPnh_pd7hvrc",
-    authDomain: "amzn-react-8f082.firebaseapp.com",
-    projectId: "amzn-react-8f082",
-    storageBucket: "amzn-react-8f082.appspot.com",
-    messagingSenderId: "133979520761",
-    appId: "1:133979520761:web:17198d298ff1262b596c28"
+  apiKey: "AIzaSyDafaMcf6XKsgWe95YZ898vPnh_pd7hvrc",
+  authDomain: "amzn-react-8f082.firebaseapp.com",
+  projectId: "amzn-react-8f082",
+  storageBucket: "amzn-react-8f082.appspot.com",
+  messagingSenderId: "133979520761",
+  appId: "1:133979520761:web:17198d298ff1262b596c28"
   };
